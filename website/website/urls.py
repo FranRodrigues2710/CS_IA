f74f24be5efe060d6508fa5ba9a59aceb552c42f
C0MPL3XXX <cisco7bat@gmail.com>
2022-10-07 12:39:39 +0100

    work

--- website/website/urls.py
+++ website/website/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 # my pages
-from sections.views import home_view
+from sections.views import home_view, contact_page
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='homepage'),
-    path('contact/', home_view, name='contactpage')
+    path('contact/', contact_page, name='contactpage')
 ]

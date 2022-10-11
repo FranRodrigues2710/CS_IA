d0c04db12402b02f05a0588d048d20cced0110db
C0MPL3XXX <cisco7bat@gmail.com>
2022-10-11 09:07:16 +0100

    work

--- website/sections/views.py
+++ website/sections/views.py
@@ -3,12 +3,10 @@ from django.http import HttpResponse
 from .models import title
 # Create your views here.
 
-# context = {'data': title.objects.all()}
-
 def home_view(request):
-    context = {'request': title.objects.all()}
+    context = {'results': title.objects.all()}
     return render(request, "mainpage.html", context)
 
 def contact_page(request):
-    context = {'request': title.objects.all()}
+    context = {'results': title.objects.all()}
     return render(request, "contacts.html", context)
